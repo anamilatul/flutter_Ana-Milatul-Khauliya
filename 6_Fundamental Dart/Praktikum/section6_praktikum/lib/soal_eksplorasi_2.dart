584abad40b4526a03b28178b2f94d0020c9efe7d
anamilatul <anamilatulaulia@gmail.com>
2023-02-22 21:26:26 +0700

    update section6_2

--- 6_Fundamental Dart/Praktikum/section6_praktikum/lib/soal_eksplorasi_2.dart
+++ 6_Fundamental Dart/Praktikum/section6_praktikum/lib/soal_eksplorasi_2.dart
@@ -17,23 +17,3 @@ void main() {
     print("$kata bukan Palindrom");
   }
 }
-
-
-
-// void main() {
-//   print("Program untuk check kata Palindrom");
-//   print("Masukkan kata yang ingin dicek : ");
-//   String kata = stdin.readLineSync()!;
-//   // print(kata);
-//   if (isPalindrome(kata)) {
-//     print("$kata adalah palindrom");
-//   } else {
-//     print("$kata bukan palindrom");
-//   }
-// }
-
-// bool isPalindrome(String kata) {
-//   String reversed = kata.split('').reversed.join();
-//   return kata == reversed;
-// }
-
