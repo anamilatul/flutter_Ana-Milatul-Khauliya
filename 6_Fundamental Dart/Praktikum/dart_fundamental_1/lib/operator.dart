240f36f3d0a93e84650017550b3c555f58fd49d2
anamilatul <anamilatulaulia@gmail.com>
2023-02-23 20:31:37 +0700

    add praktikum fundamental dart

---
+++ 6_Fundamental Dart/Praktikum/dart_fundamental_1/lib/operator.dart
@@ -0,0 +1,37 @@
+void main() {
+  //Aritmatik
+  print(1 + 3);
+  print(4 - 3);
+  print(9 * 3);
+  print(2 / 3);
+  print(5 % 2);
+
+  //Assignment
+  //   var usia = 18.0;
+//   usia = 20;
+//   print(usia);
+//   usia += 2;
+//   print(usia);
+//   usia -= 2;
+//   print(usia);
+//   usia *= 2;
+//   print(usia);
+//   usia /= 2;
+//   print(usia);
+//   usia %= 2;
+//   print(usia);
+
+//  Comparison
+//   print(1 == 2);
+//   print(1 < 2);
+//   print(1 <= 2);
+//   print(1 > 2);
+//   print(1 >= 2);
+
+//  Logical
+//print(2 == 2 && 2 == 2);
+//   print(2 == 1 || 2 == 1);
+
+//   bool notTrue = !true;
+//   print(notTrue);
+}
---
+++ 6_Fundamental Dart/Praktikum/dart_fundamental_1/lib/variabel.dart
@@ -0,0 +1,7 @@
+void main() {
+  // var usia;
+  // usia = 18;
+  //deklarasi dan memberi nilai variabel secara bersamaan
+  var usia = 18;
+  print(usia);
+}
