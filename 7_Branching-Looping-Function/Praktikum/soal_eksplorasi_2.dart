ac984c152148b53daf4ed6b1875e232ea63931a1
anamilatul <anamilatulaulia@gmail.com>
2023-02-25 14:01:01 +0700

    update section 7

--- 7_Branching-Looping-Function/Praktikum/soal_eksplorasi_2.dart
+++ 7_Branching-Looping-Function/Praktikum/soal_eksplorasi_2.dart
@@ -5,7 +5,7 @@ void main() {
   int bil = int.parse(stdin.readLineSync()!);
   for (int i = 1; i <= bil; i++) {
     for (int j = 1; j <= bil; j++) {
-      stdout.write("${i * j} ");
+      stdout.write("${i * j} \t");
     }
     stdout.write("\n");
   }
